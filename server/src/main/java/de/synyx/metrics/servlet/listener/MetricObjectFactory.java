38dfd8aa7f38037c62a41f322e97ff029d5215c7
Michael Clausen <clausen@synyx.de>
2014-08-01 09:30:10 +0200

    added simple jndi object factory to create metric registries
    * taking no configuration into account

---
+++ server/src/main/java/de/synyx/metrics/servlet/listener/MetricObjectFactory.java
@@ -0,0 +1,24 @@
+package de.synyx.metrics.servlet.listener;
+
+import com.codahale.metrics.MetricRegistry;
+
+import java.util.Hashtable;
+import javax.naming.Context;
+import javax.naming.Name;
+import javax.naming.Reference;
+import javax.naming.spi.ObjectFactory;
+
+/**
+ * Date: 01.08.2014
+ * Time: 09:09
+ */
+public final class MetricObjectFactory implements ObjectFactory {
+
+    @Override
+    public final Object getObjectInstance (Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment) throws Exception {
+        if (! (obj instanceof Reference)) return null;
+
+        return new MetricRegistry ();
+    }
+
+}
