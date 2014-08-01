5ec47a586d180c4ca5f6cb647232cf13b428316f
Michael Clausen <clausen@synyx.de>
2014-08-01 09:29:24 +0200

    added jndi and graphite handler to the mediator

--- server/src/main/java/de/synyx/metrics/servlet/listener/MetricListener.java
+++ server/src/main/java/de/synyx/metrics/servlet/listener/MetricListener.java
@@ -5,6 +5,9 @@ import com.codahale.metrics.ScheduledReporter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URI;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -15,13 +18,13 @@ import javax.servlet.ServletContextListener;
  */
 public final class MetricListener implements ServletContextListener {
 
-    private final Logger logger = LoggerFactory.getLogger (getClass ());
+    private final static Logger logger = LoggerFactory.getLogger (MetricListener.class);
 
     private final static String AttrRegistry = "com.codahale.metrics.servlet.InstrumentedFilter.registry";
     private final static String AttrReporter = "metrics-reporter";
 
     private final MetricRegistry       registry = new MetricRegistry ();
-    private final MetricReportMediator mediator = new MetricReportMediator (registry);
+    private final MetricReportMediator mediator = new MetricReportMediator (registry, jndi (), graphite ());
 
     private ScheduledReporter reporter;
 
@@ -39,6 +42,32 @@ public final class MetricListener implements ServletContextListener {
             reporter.stop ();
     }
 
+    final static MetricReportHandler jndi () {
+        try {
+            return new MetricReportHandlerJndi (new InitialContext ());
+        } catch (NamingException e) {
+            logger.error (e.getMessage (), e);
+        }
 
+        return new NoopMetricReportHandler ();
+    }
+
+    final static MetricReportHandler graphite () {
+        return new MetricReportHandlerGraphite ();
+    }
+
+    final static class NoopMetricReportHandler extends MetricReportHandler {
+
+        @Override
+        protected final String scheme () {
+            return null;
+        }
+
+        @Override
+        protected final ScheduledReporter reporter (MetricReportMediator mediator, MetricRegistry registry, URI location) {
+            return null;
+        }
+
+    }
 
 }
