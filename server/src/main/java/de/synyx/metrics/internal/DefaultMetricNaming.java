587daff672e821ab4d9bc7a1303ca95c3f41fb6a
Michael Clausen <clausen@synyx.de>
2014-08-08 13:12:50 +0200

    return $ in case of name resolution exception
    * this will give you a hint that one resolution process failed silently

--- server/src/main/java/de/synyx/metrics/internal/DefaultMetricNaming.java
+++ server/src/main/java/de/synyx/metrics/internal/DefaultMetricNaming.java
@@ -42,7 +42,7 @@ public final class DefaultMetricNaming implements MetricNaming {
                     return input.call ();
                 } catch (Exception e) {
                     logger.warn ("Partial defect occurred during name resolution: {} {}", e.getMessage (), e.getClass ());
-                    return "";
+                    return "$";
                 }
             }
 
